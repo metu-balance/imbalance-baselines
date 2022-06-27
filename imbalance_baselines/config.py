9eaf093e50f2028519b6c411e8e8fec279246553
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2022-06-27 20:05:40 +0300

    Default config path fix try

--- imbalance_baselines/config.py
+++ imbalance_baselines/config.py
@@ -3,7 +3,7 @@ from yaml import safe_load
 
 
 class Config:
-    def __init__(self, yaml_path, defaults_path="./default_config.yaml"):
+    def __init__(self, yaml_path, defaults_path="default_config.yaml"):
         with open(yaml_path, "r") as f:
             self.config = safe_load(f)
 
