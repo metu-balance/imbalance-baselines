59a1b23aacdc607cde99152fd3c3d1b13ec31065
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2023-03-06 19:01:55 +0300

    Official implementation support for focal loss

--- imbalance_baselines/loss_functions.py
+++ imbalance_baselines/loss_functions.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 
 from . import utils
 from .datasets import get_cb_weights
+from torchvision.ops import sigmoid_focal_loss
 
 
 class CostSensitiveCrossEntropy:
@@ -57,11 +58,13 @@ class InputMixup:
 
 
 class FocalLoss:
-    def __init__(self, device: torch.device = torch.device("cpu")):
-        # TODO: Should also add the option to use PyTorch's version of focal loss instead of the custom implementation
-        self.device = device
+    def __init__(self, device: torch.device = torch.device("cpu"), custom_implementation=False):
+        self.custom_implementation = custom_implementation
+
+        if not custom_implementation:
+            self.device = device
     
-    def __call__(self, z, lbl, alpha=None, gamma=0):
+    def __call__(self, z, lbl, alpha=None, gamma=0, reduction="sum"):
         """Return the focal loss tensor of shape [BATCH_SIZE] for given model & lbl.s.
 
             Args:
@@ -71,43 +74,48 @@ class FocalLoss:
                 if None is given.
               gamma: Focal loss parameter (if 0, loss is equivalent to sigmoid ce. loss)
             """
-    
-        batch_size = z.shape[0]  # Not BATCH_SIZE: The last batch might be smaller
-        lbl_cnt = z.shape[1]
-    
-        # "Decode" labels tensor to make its shape [BATCH_SIZE, label_count]:
-        lbl = F.one_hot(lbl, num_classes=lbl_cnt)
-    
-        if alpha is None:
-            alpha = torch.as_tensor([1] * batch_size, device=self.device)
-        else:  # Get cb_weights for each image in batch
-            alpha = (alpha * lbl).sum(axis=1)
-    
-        lbl_bool = lbl.type(torch.bool)  # Cast to bool for torch.where()
-        z_t = torch.where(lbl_bool, z, -z).to(self.device)
-    
-        logsig = nn.LogSigmoid()
-    
-        cross_entpy = logsig(z_t).to(self.device)
-    
-        if gamma:
-            modulator = torch.exp(
-                -gamma * torch.mul(lbl, z).to(self.device) - gamma * torch.log1p(torch.exp(-1.0 * z)).to(self.device)
+        if self.custom_implementation:
+            if reduction != "sum":
+                raise ValueError("Currently, only sum reduction is implemented in the custom focal loss.")  # TODO
+
+            batch_size = z.shape[0]  # Not BATCH_SIZE: The last batch might be smaller
+            lbl_cnt = z.shape[1]
+
+            # "Decode" labels tensor to make its shape [BATCH_SIZE, label_count]:
+            lbl = F.one_hot(lbl, num_classes=lbl_cnt)
+
+            if alpha is None:
+                alpha = torch.as_tensor([1] * batch_size, device=self.device)
+            else:  # Get cb_weights for each image in batch
+                alpha = (alpha * lbl).sum(axis=1)
+
+            lbl_bool = lbl.type(torch.bool)  # Cast to bool for torch.where()
+            z_t = torch.where(lbl_bool, z, -z).to(self.device)
+
+            logsig = nn.LogSigmoid()
+
+            cross_entpy = logsig(z_t).to(self.device)
+
+            if gamma:
+                modulator = torch.exp(
+                    -gamma * torch.mul(lbl, z).to(self.device) - gamma * torch.log1p(torch.exp(-1.0 * z)).to(self.device)
+                )
+            else:
+                modulator = 1
+
+            # Sum the value of each class in each batch. The shape is reduced from
+            #  [BATCH_SIZE, label_count] to [BATCH_SIZE].
+            unweighted_focal_loss = -torch.sum(torch.mul(modulator, cross_entpy), 1).to(
+                self.device
             )
+            weighted_focal_loss = torch.mul(alpha, unweighted_focal_loss).to(self.device)
+
+            # Normalize by the positive sample count:
+            weighted_focal_loss /= torch.sum(lbl)
+
+            return torch.sum(weighted_focal_loss)  # TODO: Implement other reduction methods as well
         else:
-            modulator = 1
-    
-        # Sum the value of each class in each batch. The shape is reduced from
-        #  [BATCH_SIZE, label_count] to [BATCH_SIZE].
-        unweighted_focal_loss = -torch.sum(torch.mul(modulator, cross_entpy), 1).to(
-            self.device
-        )
-        weighted_focal_loss = torch.mul(alpha, unweighted_focal_loss).to(self.device)
-    
-        # Normalize by the positive sample count:
-        weighted_focal_loss /= torch.sum(lbl)
-    
-        return torch.sum(weighted_focal_loss)
+            return sigmoid_focal_loss(inputs=z, targets=lbl, alpha=alpha, gamma=gamma, reduction=reduction)
 
 
 class MixupLoss:
