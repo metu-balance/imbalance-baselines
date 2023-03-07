e8d40ea2bb1df21232ca7a34fed8fe886501e3ff
BERKIN KERIM KONAR <e2375343@newinek>
2023-03-07 18:06:19 +0300

    WIP..

--- imbalance_baselines/config_class_pairing.py
+++ imbalance_baselines/config_class_pairing.py
@@ -4,6 +4,20 @@ from config import Config
 # Name of the dataset class and the file 
 # it resides in must have the same name
 # For Now...
+
+class ConfigLookUp():
+    def __init__(self, config):
+        assert config is not None
+        self.config =  config
+
+    def read_config()
+        transform = find_class('transform', config.)
+        dataset = 
+        dataloader = 
+        optimizer =
+        model = 
+        loss =   
+
 def find_class(module_name, class_name):
 
     dir = model_name + '.' + class_name
