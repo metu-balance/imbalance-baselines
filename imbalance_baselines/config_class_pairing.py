8bbadaacb92ad04261924c9aca6d7932d8277258
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2023-03-08 01:14:14 +0300

    Rename & add docstring

--- imbalance_baselines/config_class_pairing.py
+++ imbalance_baselines/config_class_pairing.py
@@ -2,29 +2,38 @@ import importlib
 
 
 def read_config(cfg):  # TODO temp reference func, may remove later
-    dataset_class = find_class('dataset', cfg.Dataset.dataset_name)
+    dataset_class = find_module_component('dataset', cfg.Dataset.dataset_name)
     dataset = dataset_class(**cfg.Dataset.dataset_parameters)
     
-    transform_class = find_class('transform', cfg.Transform.transform_name)
+    transform_class = find_module_component('transform', cfg.Transform.transform_name)
     transform = transform_class(**cfg.Transform.transform_parameters)
     
-    dataloader_class = find_class('dataloader', cfg.Dataloader.dataloader_name)
+    dataloader_class = find_module_component('dataloader', cfg.Dataloader.dataloader_name)
     dataloader = dataloader_class(**cfg.Dataloader.dataloader_parameters)
 
-    optimizer_class = find_class('optimizer', cfg.Optimizer.optimizer_name)
+    optimizer_class = find_module_component('optimizer', cfg.Optimizer.optimizer_name)
     optimizer = optimizer_class(**cfg.Optimizer.optimizer_parameters)
 
-    model_class = find_class('model', cfg.Model.model_name)
+    model_class = find_module_component('model', cfg.Model.model_name)
     model = model_class(**cfg.Model.model_parameters)
 
-    loss_class = find_class('loss', cfg.Loss.loss_name)
+    loss_class = find_module_component('loss', cfg.Loss.loss_name)
     loss = loss_class(**cfg.Loss.loss_parameters)   
 
 
-# Name of the dataset class and the file it resides in must have the same name (For Now...)
-def find_class(module_name, class_name):  # TODO: may move to utils
-    module_dir = "imbalance_baselines." + module_name + "." + class_name
+# Name of the searched field and the file it resides in must have the same name (For Now...)
+def find_module_component(module_name, component_name):  # TODO: may move to utils
+    """Finds a specified field (variable, class or function) of a given sub-module of the library.
+
+    :param module_name: Name of the sub-module, same as the name of the folder specifying it
+    :param component_name: Name of the searched component. The searched component has to have the same name
+        as the .py script in which it is defined.
+    :return: The variable, function, or the class name. Note that returned functions are not called and classes are not
+        instantiated.
+     """
+
+    module_dir = "imbalance_baselines." + module_name + "." + component_name
     module_lib = importlib.import_module(module_dir)
-    cl = getattr(module_lib, class_name)
+    cl = getattr(module_lib, component_name)
 
     return cl
