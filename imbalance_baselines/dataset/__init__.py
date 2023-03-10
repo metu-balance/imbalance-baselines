28508d7501ce854baf9056b5f043ed788f1dcbb0
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2023-03-10 18:49:06 +0300

    Temp. class name rewrite

--- imbalance_baselines/dataset/__init__.py
+++ imbalance_baselines/dataset/__init__.py
@@ -1,13 +1,14 @@
 DSET_NAMES = {
     "CIFAR10": "CIFAR10",
-    "IMB_CIFAR10": "Long-Tailed CIFAR10",
+    "CIFAR10LT": "Long-Tailed CIFAR10",
+    "INaturalist": "iNaturalist",
     "INATURALIST_2017": "iNaturalist 2017",
-    "INATURALIST_2018": "iNaturalist 2018"
+    #"INATURALIST_2018": "iNaturalist 2018"
 }
 
 DSET_CLASS_CNTS = {
     "CIFAR10": 10,
-    "IMB_CIFAR10": 10,
+    "CIFAR10LT": 10,
     "INATURALIST_2017": 5089,
     "INATURALIST_2018": 8142
 }
