e187e45536dcd13e8ea2bd33881316848f481b7c
BERKIN KERIM KONAR <e2375343@newinek>
2023-03-10 16:59:46 +0300

    WIP pipeline implementation.

--- examples/pipeline-abstract.py
+++ examples/pipeline-abstract.py
@@ -1,3 +1,35 @@
-from ../imbalance_baselines import ConfigRegistry
+from imbalance_baselines.config import Config
+from imbalance_baselines import ConfigRegistry
 
-path = "base_config"
+
+def class_balance_cal(dataset):
+    class_weights = [31, 31, 31, 31]
+    return class_weights
+
+path = "./config-abstract.yaml"
+cfg = Config(yaml_path = path)
+registar = ConfigRegistry(cfg)
+
+registar.read_config()
+
+partial_dataset = registar.partial_dataset_module
+partial_dataloader = registar.partial_dataloader_module
+partial_optimizer = registar.partial_optimizer_module
+partial_model = registar.partial_model_module
+partial_loss = registar.partial_loss_module
+
+dataset = partial_dataset()
+dataloader = partial_dataloader(dataset = dataset)
+model = partial_model()
+optimizer = partial_optimizer(params = model.parameters())
+criterion = partial_loss(weights = class_balance_cal(dataset))
+
+for epoch in range(5):
+    for (x, y) in enumerate(dataloader):
+        
+        optimizer.zero_grad()
+        out = model(x)
+        loss = criterion(out, y)
+        loss.backward()
+        optimizer.step()
+        
