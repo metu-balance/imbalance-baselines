ac92412225e87fdd5144dc42b669af62c92f0b6b
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2023-03-13 21:09:28 +0300

    Fix registry naming

--- examples/pipeline-abstract.py
+++ examples/pipeline-abstract.py
@@ -8,16 +8,16 @@ from imbalance_baselines.ConfigRegistry import Registry
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 cfg = Config(sys.argv[1])  # argv[1] should hold the path to config YAML
-registar = Registry(cfg)
+registry = Registry(cfg)
 
-partial_dataset = registar.partial_dataset_module
-partial_dataloader = registar.partial_dataloader_module
-partial_optimizer = registar.partial_optimizer_module
-partial_model = registar.partial_model_module
-partial_loss = registar.partial_loss_module
+partial_dataset = registry.partial_dataset_module
+partial_dataloader = registry.partial_dataloader_module
+partial_optimizer = registry.partial_optimizer_module
+partial_model = registry.partial_model_module
+partial_loss = registry.partial_loss_module
 
-full_train_transform = registar.full_training_transform_module
-full_test_transform = registar.full_testing_transform_module
+full_train_transform = registry.full_training_transform_module
+full_test_transform = registry.full_testing_transform_module
 
 train_dataset = partial_dataset(train=True, transform=full_train_transform)
 test_dataset = partial_dataset(train=False, transform=full_test_transform)
