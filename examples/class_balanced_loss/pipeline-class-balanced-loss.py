127775b17cf64ea5414d29bc53d1ad31dd92f185
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2023-04-05 12:31:04 +0300

    Separate focal loss from base
    + folder renaming

---
+++ examples/class_balanced_loss/pipeline-class-balanced-loss.py
@@ -0,0 +1,45 @@
+# Note that some methods such as sampling-based ones could be described in the static .yaml configuration and be used
+#   with this pipeline code.
+
+import sys
+import torch
+
+from imbalance_baselines.config import Config
+from imbalance_baselines.ConfigRegistry import Registry
+
+# TODO: Use utils' parse config?
+
+device = torch.device(
+    "cuda") if torch.cuda.is_available() else torch.device("cpu")
+cfg = Config(sys.argv[1])  # argv[1] should hold the path to config YAML
+registry = Registry(cfg, static_transofrmations=True)
+
+# Transformations are fully initialized from static parameters:
+train_transform = registry.training_transform_module
+test_transform = registry.testing_transform_module
+
+train_dataset = registry.partial_dataset_module(
+    train=True, transform=train_transform)
+test_dataset = registry.partial_dataset_module(
+    train=False, transform=test_transform)
+
+# TODO: Check dataloder config-registry format & usage...
+train_dataloader = registry.partial_dataloader_module(dataset=train_dataset)
+model = registry.partial_model_module()
+optimizer = registry.partial_optimizer_module(params=model.parameters())
+criterion = registry.partial_loss_module(dataset=train_dataset)
+
+# Intentionally simple training for test purposes only...
+for epoch in range(5):
+    print("EPOCH:", epoch)
+
+    for i, (x, y) in enumerate(train_dataloader):
+        optimizer.zero_grad()
+        out = model(x)
+        loss = criterion(out, y)
+        loss.backward()
+        optimizer.step()
+
+        print("BATCH:", i)
+
+# TODO: Extend with model save/load, evaluation, visualization (supporting wandb etc.)
